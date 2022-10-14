6f064224d7f8e57ed1281b88717a1d7e0090496b
abdurrazakosikoya <osikoyaabdurrazak@gmail.com>
2022-10-14 13:25:28 -0700

    printing single digits in base 10 from 0

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+/**
+ * main - printing single digits numbers of base 10 from 0
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int n;
+
+	for (n = 48; n < 58; n++)
+	{
+		putchar(n);
+	}
+	putchar('\n');
+	return (0);
+}
+
